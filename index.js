55b8224fa4389592b252dd4c377f1e633949852f
Zach Cullen <zacullen@gmail.com>
2020-05-05 09:35:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+
+// drivers = ['Bob', 'Mike', 'Sally']
+ function findMatching(drivers, string) {
+  return drivers.filter(d => d.toUpperCase() === string.toUpperCase())
+ }
+
+ function fuzzyMatch(drivers, string) {
+  return drivers.filter(d => d.slice(0, string.length) === string)
+ }
+
+ // drivers = [{name: 'Bob'}, ... ]
+ function matchName(drivers, string) {
+  return drivers.filter(d => d.name === string)
+ }
